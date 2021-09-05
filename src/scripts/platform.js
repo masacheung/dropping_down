aedf847cc020ce9d6b831af5ae61d589e4a8f6e7
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-09-05 16:03:33 -0400

    Can create Platform

--- src/scripts/platform.js
+++ src/scripts/platform.js
@@ -1,4 +1,10 @@
-const scale = 2;
+const CONSTANTS = {
+    SCALE: 2,
+    PLATFORMWIDTH: 150,
+    PLATFORMHEIGHT: 24,
+    PLATFORMSPEED: 2
+}
+
 
 export default class Platform {
     constructor(dimensions) {
@@ -6,11 +12,15 @@ export default class Platform {
         this.img = document.getElementById("img-bg");
         this.fireImg = document.getElementById("img-fire");
         this.wireImg = document.getElementById("img-wire");
-        this.platform = [];
+        this.platformImg = document.getElementById("img-normal");
+        this.platforms = [];
     }
 
     animate(ctx) {
         this.drawBackground(ctx);
+        // this.movePlatform();
+        this.createOnePlatform();
+        this.drawPlatform(ctx);
     }
 
     drawBackground(ctx) {
@@ -29,24 +39,26 @@ export default class Platform {
     }
 
     createOnePlatform() {
-        var platform;
-        var x = Math.random() * (this.dimensions.width - 96 * scale - 40 * scale) + 20 * scale;
-        var y = this.dimensions.height;
-        var rand = Math.random() * 100;
+        let platform;
+        let x = Math.random() * 800;
+        let y = Math.random() * 800;
+        let rand = Math.random() * 100;
 
         let platformType = "normal";
-        if (rand < 50) {
-            this.platform.push([x,y,"normal"])
-        }else if (rand < 60) {
-            this.platform.push([x,y,"nails"]);
-            platformType = "nails";
-        }else if (ramd <80) {
-            this.platform.push([x,y,"jump"]);
-            platformType = "jump";
-        }else {
-            this.platform.push([x,y,"fake"]);
-            platformType = "fake";
-        }
+        // if (rand < 50) {
+        //     platform = [x,y,"normal"];
+        // }else if (rand < 60) {
+        //     platform = [x,y,"nails"];
+        //     platformType = "nails";
+        // }else if (ramd <80) {
+        //     platform = [x,y,"jump"];
+        //     platformType = "jump";
+        // }else {
+        //     platform = [x,y,"fake"];
+        //     platformType = "fake";
+        // }
+
+        this.platforms.push([x,y]);
     }
 
     movePlatform() {
@@ -54,6 +66,8 @@ export default class Platform {
     }
 
     drawPlatform(ctx) {
-
+        this.platforms.forEach((ele) => {
+            ctx.drawImage(this.platformImg, ele[0], ele[1], CONSTANTS.PLATFORMWIDTH, CONSTANTS.PLATFORMHEIGHT);
+        });
     }
 }
