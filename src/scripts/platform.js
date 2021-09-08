9f62176cf43bdacd4de7184ee65306f88c0ade41
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-09-08 14:27:01 -0400

    adding meun

--- src/scripts/platform.js
+++ src/scripts/platform.js
@@ -55,9 +55,8 @@ export default class Platform {
             ctx.fillStyle = "#FC46AA";
             ctx.textAlign = "center";
             ctx.drawImage(this.startImg, 300, 200, this.dimensions.width/4 ,this.dimensions.height/4);
-            ctx.fillText("Click Here to Play.", this.dimensions.width/2, this.dimensions.height - 350);
-            ctx.fillText("Click To Pause.", this.dimensions.width/2, this.dimensions.height - 325);
-            ctx.fillText("'Everytime Pause the Game, the charater will move faster!!!'", this.dimensions.width/2, this.dimensions.height - 300);
+            ctx.fillText("Click to Start. Click To Pause.", this.dimensions.width/2, this.dimensions.height - 350);
+            ctx.fillText("'Everytime Pause the Game, the charater will move faster!!!'", this.dimensions.width/2, this.dimensions.height - 315);
             ctx.fillText("Moving Left Using [W] OR [←].", this.dimensions.width/2, this.dimensions.height - 270);
             ctx.fillText("Moving Right Using [D] OR [→].", this.dimensions.width/2, this.dimensions.height - 240);
             // ctx.fillText("Moving Left Using [W] || [←]. Moving Right Using [D] || [→].")
